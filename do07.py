81dee034f7426af69dce52e7c282f3fbfb143bb8
Klaus Johannsen <kljo@norceresearch.no>
2022-08-10 11:26:39 +0200

    ggg

--- do07.py
+++ do07.py
@@ -8,13 +8,10 @@ from modules.points import circle, xline
 from modules.plot import plot2d, plot3d
 
 # run
-#X = circle()
-#X = xline(shift = [1, 1])
-#R = rel.distance_relation(X)
+X = circle()
+R = rel.distance_relation(X)
 #R = rel.scp_relation(X) 
 #R = rel.binary_relation(R, threshold = 0.5, r = '>')
-
-R = rel.countries_1()
 
 U, S, VT = linalg.svd(R, n = 4, verbose = True)
 XX = U @ S
